dc6331c4c39f3a56368e45772820d9bd7fc779aa
sanghyunkim <sanghyunkim25@gmail.com>
2025-10-25 18:12:57 +0900

    init project

---
+++ spec_alpha/gcc/gcc_input/200.s
@@ -0,0 +1,621 @@
+	.file	"200.i"
+	.data
+	.align 8
+	.type	c__12,@object
+	.size	c__12,8
+c__12:
+	.quad	12
+	.align 8
+	.type	c__1,@object
+	.size	c__1,8
+c__1:
+	.quad	1
+	.align 8
+	.type	c__0,@object
+	.size	c__0,8
+c__0:
+	.quad	0
+	.align 8
+	.type	c__11,@object
+	.size	c__11,8
+c__11:
+	.quad	11
+	.align 8
+	.type	c__13,@object
+	.size	c__13,8
+c__13:
+	.quad	13
+	.align 8
+	.type	c__2,@object
+	.size	c__2,8
+c__2:
+	.quad	2
+	.align 8
+	.type	c__6,@object
+	.size	c__6,8
+c__6:
+	.quad	6
+	.align 8
+	.type	c__9,@object
+	.size	c__9,8
+c__9:
+	.quad	9
+	.align 8
+	.type	c__36,@object
+	.size	c__36,8
+c__36:
+	.quad	36
+	.align 8
+	.type	c__14,@object
+	.size	c__14,8
+c__14:
+	.quad	14
+	.align 8
+	.type	c__4,@object
+	.size	c__4,8
+c__4:
+	.quad	4
+	.align 8
+	.type	c_b251,@object
+	.size	c_b251,8
+c_b251:
+	.long	0
+	.long	0
+	.align 8
+	.type	c__5,@object
+	.size	c__5,8
+c__5:
+	.quad	5
+	.align 8
+	.type	c__84,@object
+	.size	c__84,8
+c__84:
+	.quad	84
+	.align 8
+	.type	c__600,@object
+	.size	c__600,8
+c__600:
+	.quad	600
+	.align 8
+	.type	c__3,@object
+	.size	c__3,8
+c__3:
+	.quad	3
+	.align 8
+	.type	c_b461,@object
+	.size	c_b461,8
+c_b461:
+	.long	0
+	.long	1071644672
+	.align 8
+	.type	c_b464,@object
+	.size	c_b464,8
+c_b464:
+	.long	0
+	.long	3219128320
+	.align 8
+	.type	c__100,@object
+	.size	c__100,8
+c__100:
+	.quad	100
+	.align 8
+	.type	c__85,@object
+	.size	c__85,8
+c__85:
+	.quad	85
+	.align 8
+	.type	c__58,@object
+	.size	c__58,8
+c__58:
+	.quad	58
+	.align 8
+	.type	c__57,@object
+	.size	c__57,8
+c__57:
+	.quad	57
+	.align 8
+	.type	c__15,@object
+	.size	c__15,8
+c__15:
+	.quad	15
+	.align 8
+	.type	c__16,@object
+	.size	c__16,8
+c__16:
+	.quad	16
+	.align 8
+	.type	c__59,@object
+	.size	c__59,8
+c__59:
+	.quad	59
+	.align 8
+	.type	c__17,@object
+	.size	c__17,8
+c__17:
+	.quad	17
+	.align 8
+	.type	c__18,@object
+	.size	c__18,8
+c__18:
+	.quad	18
+	.align 8
+	.type	c__26,@object
+	.size	c__26,8
+c__26:
+	.quad	26
+	.align 8
+	.type	c__19,@object
+	.size	c__19,8
+c__19:
+	.quad	19
+	.align 8
+	.type	c__20,@object
+	.size	c__20,8
+c__20:
+	.quad	20
+	.align 8
+	.type	c__21,@object
+	.size	c__21,8
+c__21:
+	.quad	21
+	.align 8
+	.type	c__40,@object
+	.size	c__40,8
+c__40:
+	.quad	40
+	.align 8
+	.type	c__91,@object
+	.size	c__91,8
+c__91:
+	.quad	91
+	.align 8
+	.type	c__52,@object
+	.size	c__52,8
+c__52:
+	.quad	52
+	.align 8
+	.type	c__65,@object
+	.size	c__65,8
+c__65:
+	.quad	65
+	.align 8
+	.type	c__67,@object
+	.size	c__67,8
+c__67:
+	.quad	67
+	.align 8
+	.type	c__66,@object
+	.size	c__66,8
+c__66:
+	.quad	66
+	.align 8
+	.type	c__45,@object
+	.size	c__45,8
+c__45:
+	.quad	45
+	.align 8
+	.type	c__81,@object
+	.size	c__81,8
+c__81:
+	.quad	81
+	.align 8
+	.type	c__27,@object
+	.size	c__27,8
+c__27:
+	.quad	27
+	.align 8
+	.type	c__25,@object
+	.size	c__25,8
+c__25:
+	.quad	25
+	.align 8
+	.type	c__28,@object
+	.size	c__28,8
+c__28:
+	.quad	28
+	.align 8
+	.type	c__55,@object
+	.size	c__55,8
+c__55:
+	.quad	55
+	.align 8
+	.type	c_b1102,@object
+	.size	c_b1102,8
+c_b1102:
+	.quad	300000
+	.align 8
+	.type	c__80,@object
+	.size	c__80,8
+c__80:
+	.quad	80
+	.align 8
+	.type	c__86,@object
+	.size	c__86,8
+c__86:
+	.quad	86
+	.align 8
+	.type	c__87,@object
+	.size	c__87,8
+c__87:
+	.quad	87
+	.align 8
+	.type	c__29,@object
+	.size	c__29,8
+c__29:
+	.quad	29
+	.align 8
+	.type	c__8,@object
+	.size	c__8,8
+c__8:
+	.quad	8
+	.align 8
+	.type	c__44,@object
+	.size	c__44,8
+c__44:
+	.quad	44
+	.align 8
+	.type	c__83,@object
+	.size	c__83,8
+c__83:
+	.quad	83
+	.align 8
+	.type	c__82,@object
+	.size	c__82,8
+c__82:
+	.quad	82
+	.align 8
+	.type	c__92,@object
+	.size	c__92,8
+c__92:
+	.quad	92
+	.align 8
+	.type	c__37,@object
+	.size	c__37,8
+c__37:
+	.quad	37
+	.align 8
+	.type	c__46,@object
+	.size	c__46,8
+c__46:
+	.quad	46
+	.align 8
+	.type	c__47,@object
+	.size	c__47,8
+c__47:
+	.quad	47
+	.align 8
+	.type	c__48,@object
+	.size	c__48,8
+c__48:
+	.quad	48
+	.align 8
+	.type	c__49,@object
+	.size	c__49,8
+c__49:
+	.quad	49
+	.align 8
+	.type	c__50,@object
+	.size	c__50,8
+c__50:
+	.quad	50
+	.align 8
+	.type	c__7,@object
+	.size	c__7,8
+c__7:
+	.quad	7
+	.align 8
+	.type	c__39,@object
+	.size	c__39,8
+c__39:
+	.quad	39
+	.align 8
+	.type	c__51,@object
+	.size	c__51,8
+c__51:
+	.quad	51
+	.align 8
+	.type	c__62,@object
+	.size	c__62,8
+c__62:
+	.quad	62
+	.align 8
+	.type	c__63,@object
+	.size	c__63,8
+c__63:
+	.quad	63
+	.align 8
+	.type	c__78,@object
+	.size	c__78,8
+c__78:
+	.quad	78
+	.align 8
+	.type	c__70,@object
+	.size	c__70,8
+c__70:
+	.quad	70
+	.align 8
+	.type	c__71,@object
+	.size	c__71,8
+c__71:
+	.quad	71
+	.align 8
+	.type	c__72,@object
+	.size	c__72,8
+c__72:
+	.quad	72
+	.align 8
+	.type	c__73,@object
+	.size	c__73,8
+c__73:
+	.quad	73
+	.align 8
+	.type	c__74,@object
+	.size	c__74,8
+c__74:
+	.quad	74
+	.align 8
+	.type	c__75,@object
+	.size	c__75,8
+c__75:
+	.quad	75
+	.align 8
+	.type	c__76,@object
+	.size	c__76,8
+c__76:
+	.quad	76
+	.align 8
+	.type	c__77,@object
+	.size	c__77,8
+c__77:
+	.quad	77
+	.align 8
+	.type	c__88,@object
+	.size	c__88,8
+c__88:
+	.quad	88
+	.align 8
+	.type	c__41,@object
+	.size	c__41,8
+c__41:
+	.quad	41
+	.align 8
+	.type	c__42,@object
+	.size	c__42,8
+c__42:
+	.quad	42
+	.align 8
+	.type	c__54,@object
+	.size	c__54,8
+c__54:
+	.quad	54
+	.align 8
+	.type	c__64,@object
+	.size	c__64,8
+c__64:
+	.quad	64
+	.align 8
+	.type	c_b2366,@object
+	.size	c_b2366,8
+c_b2366:
+	.long	0
+	.long	1072693248
+	.align 8
+	.type	c_b2381,@object
+	.size	c_b2381,8
+c_b2381:
+	.long	0
+	.long	3220176896
+	.align 8
+	.type	c_b2382,@object
+	.size	c_b2382,8
+c_b2382:
+	.long	0
+	.long	1084178432
+	.align 8
+	.type	c_b2384,@object
+	.size	c_b2384,8
+c_b2384:
+	.long	0
+	.long	1083129856
+	.align 8
+	.type	c_b2400,@object
+	.size	c_b2400,8
+c_b2400:
+	.long	0
+	.long	1073741824
+	.align 8
+	.type	c_b2414,@object
+	.size	c_b2414,8
+c_b2414:
+	.long	0
+	.long	1085227008
+	.align 8
+	.type	c_b2415,@object
+	.size	c_b2415,8
+c_b2415:
+	.long	0
+	.long	1074790400
+	.align 8
+	.type	c__33,@object
+	.size	c__33,8
+c__33:
+	.quad	33
+	.align 8
+	.type	c_b2856,@object
+	.size	c_b2856,8
+c_b2856:
+	.long	2233382994
+	.long	1075139051
+	.align 8
+	.type	c_b2858,@object
+	.size	c_b2858,8
+c_b2858:
+	.long	3264175145
+	.long	1074866421
+	.align 8
+	.type	c_b2862,@object
+	.size	c_b2862,8
+c_b2862:
+	.long	2576980378
+	.long	1074370969
+	.align 8
+	.type	c_b2869,@object
+	.size	c_b2869,8
+c_b2869:
+	.long	1346542433
+	.long	1072827863
+	.align 8
+	.type	c__700,@object
+	.size	c__700,8
+c__700:
+	.quad	700
+	.align 8
+	.type	c__300,@object
+	.size	c__300,8
+c__300:
+	.quad	300
+	.align 8
+	.type	c_b3071,@object
+	.size	c_b3071,8
+c_b3071:
+	.quad	280000
+	.align 8
+	.type	c__500,@object
+	.size	c__500,8
+c__500:
+	.quad	500
+	.align 8
+	.type	c__10,@object
+	.size	c__10,8
+c__10:
+	.quad	10
+	.align 8
+	.type	c_n1,@object
+	.size	c_n1,8
+c_n1:
+	.quad	-1
+	.align 8
+	.type	c_b4017,@object
+	.size	c_b4017,8
+c_b4017:
+	.long	0
+	.long	1074266112
+	.align 8
+	.type	c_b4018,@object
+	.size	c_b4018,8
+c_b4018:
+	.long	0
+	.long	1075052544
+	.align 8
+	.type	c_b4019,@object
+	.size	c_b4019,8
+c_b4019:
+	.long	0
+	.long	1075576832
+	.align 8
+	.type	c__90,@object
+	.size	c__90,8
+c__90:
+	.quad	90
+	.align 8
+	.type	c__20000,@object
+	.size	c__20000,8
+c__20000:
+	.quad	20000
+	.align 8
+	.type	c_n20,@object
+	.size	c_n20,8
+c_n20:
+	.quad	-20
+	.align 8
+	.type	c_n111,@object
+	.size	c_n111,8
+c_n111:
+	.quad	-111
+	.align 8
+	.type	c_b5817,@object
+	.size	c_b5817,8
+c_b5817:
+	.long	858993459
+	.long	1069757235
+	.align 8
+	.type	c__30000,@object
+	.size	c__30000,8
+c__30000:
+	.quad	30000
+	.align 8
+	.type	c__8400,@object
+	.size	c__8400,8
+c__8400:
+	.quad	8400
+	.align 8
+	.type	c__3600,@object
+	.size	c__3600,8
+c__3600:
+	.quad	3600
+	.align 8
+	.type	c__7700,@object
+	.size	c__7700,8
+c__7700:
+	.quad	7700
+	.align 8
+	.type	c__2100,@object
+	.size	c__2100,8
+c__2100:
+	.quad	2100
+	.align 8
+	.type	c_b6183,@object
+	.size	c_b6183,8
+c_b6183:
+	.quad	800000
+	.align 8
+	.type	c__60000,@object
+	.size	c__60000,8
+c__60000:
+	.quad	60000
+	.align 8
+	.type	c__1200,@object
+	.size	c__1200,8
+c__1200:
+	.quad	1200
+	.align 8
+	.type	c__2000,@object
+	.size	c__2000,8
+c__2000:
+	.quad	2000
+	.align 8
+	.type	c__8100,@object
+	.size	c__8100,8
+c__8100:
+	.quad	8100
+	.align 8
+	.type	c__1400,@object
+	.size	c__1400,8
+c__1400:
+	.quad	1400
+	.align 8
+	.type	c__89600,@object
+	.size	c__89600,8
+c__89600:
+	.quad	89600
+	.align 8
+	.type	c_b6341,@object
+	.size	c_b6341,8
+c_b6341:
+	.quad	537600
+	.align 8
+	.type	c_b6360,@object
+	.size	c_b6360,8
+c_b6360:
+	.quad	220000
+	.align 8
+	.type	c__44800,@object
+	.size	c__44800,8
+c__44800:
+	.quad	44800
+	.align 8
+	.type	c__128,@object
+	.size	c__128,8
+c__128:
+	.quad	128
+	.align 8
